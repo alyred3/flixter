244d7f3441de09e70eb7c7cb76299b1e2dd4ef3b
Alyssa Redman <alyred3@gmail.com>
2019-09-13 00:49:59 +0000

    set up video uploading

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -25,7 +25,7 @@ private
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,11 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+    if Rails.env.development?
+  config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+  config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+  
   config.storage = :aws                       
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl = "public-read"
@@ -10,4 +15,6 @@ CarrierWave.configure do |config|
     secret_access_key: ENV["AWS_SECRET_KEY"],
     region: ENV["AWS_REGION"]        
   }
+
 end
+
